ffac3022a2c5135f16765646a0a5337ed839c7b6
DanielSalazar1115 <64703693+DanielSalazar1115@users.noreply.github.com>
2021-03-17 22:23:26 -0400

    Add files via upload

--- image_batches.py
+++ image_batches.py
@@ -3,12 +3,15 @@ from PIL import Image
 from PIL import ImageFilter
 import matplotlib.pyplot as plt
 import numpy as np 
-%matplotlib inline
 import glob
 import pandas as pd
 import time
 import random
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# %%
 
 # %%
 df = pd.read_csv("food101/meta/meta/classes.txt")
@@ -16,6 +19,7 @@ class_list = list(df["apple_pie"])
 class_list = ["apple_pie"] + class_list
 df = pd.DataFrame(class_list)
 df
+
 # %%
 base_path = "food101/images/"
 image_paths = []
@@ -45,17 +49,22 @@ def get_batch(size):
     paths = random.sample(list(image_paths), size)
     X = [np.asarray(Image.open(p)) for p in paths]
     Y = [label_dict[get_label(p)] for p in paths]
-    return X, Y
+    return X, y
+
+#Image.fromarray(X[0])
 # %%
 #image transforming/ preprocessing, takes in images in the form of arrays for 
 # training data
+
+X, y = get_batch(100)
+
 def train_preprocess(image_arrs):
     ims = []
     angles = [30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360]
     for im in image_arrs:
         i = Image.fromarray(im)
         #first convert all images to one standard size
-        i = i.resize((200, 200))
+        i = i.resize((32, 32))
         #randomly flip images horizontally and vertically
         flipper = random.randint(1, 3)
         if flipper == 2:
@@ -75,9 +84,28 @@ def train_preprocess(image_arrs):
     
     return ims
 
+imy = train_preprocess(X)
+
+imy[2].shape
+
+
+# %%
+X, y = get_batch(100)
+
+def simple_normalize(image_arrs):
+    ims = []
+    for i in image_arrs:
+        i = i/255
+        ims.append(i)
+    return ims
+
+imy = simple_normalize(X)
+
+
 
 # %%
 #function to normalize all image values, using local centering
+
 def norm_images(image_arrs):
     ims = []
     for i in image_arrs:
@@ -86,13 +114,49 @@ def norm_images(image_arrs):
         i -= means
         ims.append(i)
     return ims
-
-
 # %%
 #run small test
-X, Y = get_batch(10000)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 42)
-prepdx = train_preprocess(X)
-normdtrain = norm_images(prepdx)
-normdtest = norm_images(x_test)
+X, y = get_batch(10000)
+X = train_preprocess(X)
+X = simple_normalize(X)
+X = norm_images(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
+X_train, X_test, y_train, y_test = np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
 # %%
+
+# %%
+
+# create basic neural network
+
+model = models.Sequential()
+model.add(layers.Conv2D(100, (3,3), activation = 'relu', input_shape = (32, 32, 3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation= 'relu'))
+model.add(layers.Dense(101))
+
+model.summary()
+
+# compile and train the model
+
+model.compile(optimizer='adam', 
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10,
+    validation_data=(X_test, y_test))
+
+# %%
+
+# plot train vs test accuracy
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
